74d480f5a09fd31c75b3aaa7d88ffdbbd3104157
Julio Rdz <juliordzcer@outlook.com>
2025-02-26 11:24:05 -0600

    Se agrego el directorio de bebop_gz directamente a este repositorio

---
+++ bebop_gz/plugins/build/CMakeFiles/SetPosePlugin.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SetPosePlugin.dir/SetPosePlugin.cc.o"
+  "CMakeFiles/SetPosePlugin.dir/SetPosePlugin.cc.o.d"
+  "libSetPosePlugin.pdb"
+  "libSetPosePlugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SetPosePlugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
