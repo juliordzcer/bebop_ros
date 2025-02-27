ce87f37d90f2d413975afcf93845edbe03167aa7
Julio Rdz <juliordzcer@outlook.com>
2025-02-26 20:12:05 -0600

    Codifos funcionando

--- bebop_demo/launch/indiviual.launch.py
+++ bebop_demo/launch/indiviual.launch.py
@@ -15,7 +15,8 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')),
         launch_arguments={
-            'gz_args': '-r -z 1000000 bebop.sdf'
+            # 'gz_args': '-r -z 1000000 bebop.sdf'
+            'gz_args': '-r --headless-rendering -z 1000000 bebop.sdf'
         }.items(),
     )
 
