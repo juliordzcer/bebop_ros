9e7f133c148525ba20b05892e1f14c3678d508b1
Julio Rdz <juliordzcer@outlook.com>
2025-02-26 20:16:39 -0600

    a

--- bebop_demo/launch/indiviual.launch.py
+++ bebop_demo/launch/indiviual.launch.py
@@ -16,7 +16,7 @@ def generate_launch_description():
             os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')),
         launch_arguments={
             # 'gz_args': '-r -z 1000000 bebop.sdf'
-            'gz_args': '-r --headless-rendering -z 1000000 bebop.sdf'
+            'gz_args': '-r -s -z 1000000 bebop.sdf'
         }.items(),
     )
 
