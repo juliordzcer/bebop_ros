46d5c40bd32ba33f945b99401001a473a355dbd3
Julio Rdz <juliordzcer@outlook.com>
2025-03-08 22:58:38 -0600

    Se intento con xacro

--- bebop_gz/setup.py
+++ bebop_gz/setup.py
@@ -16,6 +16,7 @@ setup(
         (os.path.join('share', package_name, 'models'), glob('models/****/***/**/*')),
         (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
